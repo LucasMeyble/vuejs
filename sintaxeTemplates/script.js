f7c5e3540ece0e5154e06d72ad690e0d3cc67630
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-09 16:24:20 -0300

    sintaxe de template: interpolações

---
+++ sintaxeTemplates/script.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+	el:'#app',
+	data:{
+		titulo: 'titulo',
+		subtitle: 'este é o <strong> subtitle </strong>da pagina',
+		copyRight: 'Todos os direitos reservados',
+		nome: 'lucas',
+		carregado: true
+	}
+});
