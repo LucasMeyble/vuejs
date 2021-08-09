51bc75930a825eedec791e783e08d21dc3dd511b
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-09 17:04:59 -0300

    sintexe de template: directivas

--- sintaxeTemplates/script.js
+++ sintaxeTemplates/script.js
@@ -8,3 +8,21 @@ let app = new Vue({
 		carregado: true
 	}
 });
+
+var app2 = new Vue({
+	el: '#app-2',
+	data: {
+		aviso: true,
+		div_id: 'teste',
+		link: 'https://www.adasi.com.br/',
+		id: 14
+	},
+	methods: {
+		clicou: () => {
+			alert('Evento de click')
+		},
+		enviou: () => {
+			alert('ENVIOU!! 😁')
+		}
+	}
+})
