2294504b7c67869c93be72cf1796d58636dc3f42
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-16 17:59:55 -0300

    Executando funções fora do componente

--- script.js
+++ script.js
@@ -123,4 +123,38 @@ var conteudo2 = new Vue({
     }
 })
 
+// ---------------------------------------------------------------------------------
+
+var postagem = {
+    props: ['titulo'],
+    methods: {
+        responder: function(){
+            this.$emit('responder', this.titulo)
+        }
+    },
+    template: `
+        <div>
+            <h2>{{titulo}}</h2>
+            <p><slot></solot></p>
+            <button v-on:click="responder">Responder</button>
+        </div>
+    `
+};
+
+var conteudo3 = new Vue({
+    el: '#conteudo3',
+    data: {
+        responderAberto: false,
+        respostaTitulo: ''
+    },
+    methods: {
+        abrirResposta: function(titulo){
+            this.responderAberto = true;
+            this.respostaTitulo = titulo;
+        }
+    },
+    components: {
+        postagem: postagem
+    }
+})
 
