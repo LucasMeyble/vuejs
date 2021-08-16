f7eaf96c505c6fd5c2f4dfd12bc9c532a88f641b
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-16 11:45:09 -0300

    componente global vs local

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var app = new Vue({
     data: {}
 })
 
+// ---------------------------------------------------------------------------------
+
 Vue.component('contador', {
     data: function(){
         return {
@@ -34,3 +36,50 @@ var app1 = new Vue({
     el: '#app-1',
     data: {}
 })
+
+// ---------------------------------------------------------------------------------
+
+Vue.component('contador', {
+    data: function(){
+        return{
+            c: 0
+        };
+    },
+    methods: {
+        aumentar: function(){
+            this.c++;
+        }
+    },
+    template: 
+    `<div>
+        <span>{{c}}</span>
+        <button v-on:click="aumentar">Aumentar</button>    
+    </div>`
+})
+
+var menuSuperior = {
+    template: '<span>MENU</span>'
+}
+
+var header = new Vue({
+    el: '#header',
+    data: {
+        title: 'Titulo do site'
+    },
+    components: {
+        'menu-superior': menuSuperior
+    }
+})
+
+var sidebar = new Vue({
+    el: '#sidebar',
+    data: {
+        item: 'Alguma coisa'
+    }
+})
+
+
+
+
+
+
