609a4592645805c38bf4df451017c9595978e9da
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-27 16:49:45 -0300

    atributos dinamicos

--- documentation/src/components/info.vue
+++ documentation/src/components/info.vue
@@ -9,17 +9,24 @@
             <li>Pyton </li>
         </ul>
         <p v-show="mostrarEmail">mande uma mensagem para: {{email}}</p>
+        <p>Para acessar meu portifolio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <Picture/>
     </div>
 </template>
 
 <script>
+import Picture from './picture.vue';
 export default {
     name: 'info',
+    components: {
+        Picture
+    },
     data() {
         return{
             estaTrabalhando: false,
             mostrarEmail: true,
-            email: 'Lucasmeyble123@gmail.com'
+            email: 'Lucasmeyble123@gmail.com',
+            meu_link: 'https://google.com'
         }
     }
 }
