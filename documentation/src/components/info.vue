d863187e3720fd6fe64da822031b2ffadfc70333
lucas_meyble <lucasmeyble123@gmail.com>
2021-08-28 14:07:49 -0300

    metodos

--- documentation/src/components/info.vue
+++ documentation/src/components/info.vue
@@ -9,7 +9,13 @@
             <li>Pyton </li>
         </ul>
         <p v-show="mostrarEmail">mande uma mensagem para: {{email}}</p>
-        <p>Para acessar meu portifolio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <!-- o dois pontos é equivalente a v-bind: -->
+        <p>Para acessar meu portifolio <a :href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <div>
+            <button @click="showEmail">
+                {{textoBotao}}
+            </button>
+        </div>
         <Picture/>
     </div>
 </template>
@@ -24,9 +30,20 @@ export default {
     data() {
         return{
             estaTrabalhando: false,
-            mostrarEmail: true,
+            mostrarEmail: false,
             email: 'Lucasmeyble123@gmail.com',
-            meu_link: 'https://google.com'
+            meu_link: 'https://google.com',
+            textoBotao: 'mostrar email'
+        }
+    }, 
+    methods: {
+        showEmail(){
+            this.mostrarEmail = !this.mostrarEmail;
+            if(!this.mostrarEmail){
+                this.textoBotao = 'mostar email'
+            }else{
+                this.textoBotao = 'Ocultar email'
+            }
         }
     }
 }
