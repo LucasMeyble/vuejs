b6f847f8afcf1d22cb208acb21dd62bf19efaed3
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-30 11:09:04 -0300

    multiplos eventos

--- documentation/src/components/firstComponent.vue
+++ documentation/src/components/firstComponent.vue
@@ -2,13 +2,19 @@
 <div>
     <h1>Ola Vue</h1>
     <h2>{{title}}</h2>
+    <multiplosEventos />
 </div>
 </template>
 
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
+
 export default {
     name: 'firstComponent',
+    components: {
+        MultiplosEventos
+    },
     props: {
         title: String
     }
