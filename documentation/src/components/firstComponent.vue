e938d3ed16459731fa740b21029ec12950444072
Lucas Meyble <lucasmeyble123@gmail.com>
2021-08-30 11:48:03 -0300

    reutilização de componentes

--- documentation/src/components/firstComponent.vue
+++ documentation/src/components/firstComponent.vue
@@ -3,17 +3,21 @@
     <h1>Ola Vue</h1>
     <h2>{{title}}</h2>
     <multiplosEventos />
+    <Reutilizacao />
+    <Reutilizacao />
 </div>
 </template>
 
 
 <script>
-import MultiplosEventos from './MultiplosEventos.vue'
+import MultiplosEventos from './MultiplosEventos.vue';
+import Reutilizacao from './Reutilizacao.vue';
 
 export default {
     name: 'firstComponent',
     components: {
-        MultiplosEventos
+        MultiplosEventos,
+        Reutilizacao
     },
     props: {
         title: String
